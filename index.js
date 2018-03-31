9385c2af9e64bdcd72b439e91959db56f4ed615b
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-31 00:18:42 -0700

    Remove unnecessary url var

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var request = request.defaults({strictSSL: false, jar: true});
 let host = "https://sc2-rdops-vm06-dhcp-195-173";
 
 let username = 'administrator@vsphere.local',
-    password = 'Admin!23',
-    url = `${host}/rest/com/vmware/cis/session`;
+    password = 'Admin!23';
 
 async function listVms() {
   return await request({ url: `${host}/rest/vcenter/vm` });
@@ -14,7 +13,7 @@ async function listVms() {
 
 // Basic Auth to vSphere REST Endpoint, uses cookie for authentication
 request({
-  url : url,
+  url : `${host}/rest/com/vmware/cis/session`,
   method: 'POST',
   headers : {
     "Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64")
