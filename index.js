690e3cd7c0da162eef3d46654b3384fa7d588782
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-31 00:57:23 -0700

    Minor change to improve the error handling.

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var request = require('request-promise-native');
+
+// Disable SSL for simplicity and enable the cookie jar for REST API session ID
 request = request.defaults({strictSSL: false, jar: true});
 
 let host = "https://sc2-rdops-vm06-dhcp-195-173";
 let username = 'administrator@vsphere.local';
 let password = 'Admin!23';
 
+// Generic HTTP GET
 async function get(url) {
   return await request({ url: url });
 }
 
+// Login using Basic Auth
 async function login() {
   await request({
     url : `${host}/rest/com/vmware/cis/session`,
@@ -17,11 +21,14 @@ async function login() {
   });
 }
 
+// Make a few API calls
 async function callApi() {
+
   // Fetch a list of VMs
   let vms = JSON.parse(await get(`${host}/rest/vcenter/vm`));
   console.log(JSON.stringify(vms));
-  // Fetch details for the first vm
+
+  // Fetch details of the first vm in the list
   if(vms.value.length > 0) {
     let vm = await get(`${host}/rest/vcenter/vm/${vms.value[0].vm}`);
     console.log(`\nDetails of VM: ${vms.value[0].vm}`);
@@ -32,5 +39,9 @@ async function callApi() {
 login().then(() => {
   callApi();
 }, (error) => {
-  console.log(error);
+  if(error.response) {
+    console.log(error.response.statusMessage);
+  } else {
+    console.log(error.message);
+  }
 });
