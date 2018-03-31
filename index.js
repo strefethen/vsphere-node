98721193713340ed0c750555faa173f8f3e71fdd
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-31 00:16:57 -0700

    Switch to using request-promise-native to simplify the code and error handling.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var request = require('request');
+var request = require('request-promise-native');
 
 var request = request.defaults({strictSSL: false, jar: true});
 
@@ -8,37 +8,27 @@ let username = 'administrator@vsphere.local',
     password = 'Admin!23',
     url = `${host}/rest/com/vmware/cis/session`;
 
-function listVms() {
-  request({
-    url: `${host}/rest/vcenter/vm`,
-    }, function(error, response, body) {
-      if(error) {
-        console.log(`Error: ${error.message}`);
-      } else if(response.statusCode == 200) {
-        console.log(body);
-      } else {
-        console.log(`Error: ${response.statusMessage}`);
-      }
-    }
-  );
+async function listVms() {
+  return await request({ url: `${host}/rest/vcenter/vm` });
 }
 
 // Basic Auth to vSphere REST Endpoint, uses cookie for authentication
 request({
-    url : url,
-    method: 'POST',
-    headers : {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      "Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64")
-    }
-  }, function (error, response, body) {
-    if(error) {
-      console.log(`Error: ${error.message}`);
-    } else if(response.statusCode == 200) {
-      listVms();
-    } else {
-      console.log(`Error: ${response.statusMessage}`);
-    }
+  url : url,
+  method: 'POST',
+  headers : {
+    "Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64")
   }
-);
+}).then((value) => {
+  listVms().then((vms) => { 
+    console.log(vms);
+  }, (error) => {
+    console.log(error);
+  });
+}, (error) => {
+  if(error.response) {
+    console.log(error.response.statusMessage);
+  } else {
+    console.log(error.message);
+  }
+});
