bbdf8f98b9a400dd49c0712346c9439625951969
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-30 23:03:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vSphere-Node
+Illustrates calling the vSphere REST API via nodejs
---
+++ index.js
@@ -0,0 +1,35 @@
+var request = require('request');
+
+let host = "https://sc2-rdops-vm06-dhcp-195-173";
+
+let username = 'administrator@vsphere.local',
+    password = 'Admin!23',
+    url = `${host}/rest/com/vmware/cis/session`;
+
+request.defaults({ strictSSL: false});
+
+function listVms() {
+	request({
+		url: `${host}/rest/vcenter/vm`,
+		strictSSL: false,
+		jar: true 
+	}, function(error, response, body) {
+		console.log(body);
+	});
+}
+
+// Basic Auth to vSphere REST Endpoint, uses cookie for authentication
+request({
+		url : url,
+		method: 'POST',
+		strictSSL: false,
+		jar: true,
+		headers : {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+				"Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64")
+		}
+  }, function (error, response, body) {
+   listVms();
+  }
+);
