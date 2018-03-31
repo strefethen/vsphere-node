6fcdf8ed22dd4af3bd0ed3f0aaa48181f4f92a19
Steve Trefethen <strefethen@users.noreply.github.com>
2018-03-31 00:48:07 -0700

    Refactored login and fetch routines to use a generic "get" function and allow error handling to be reused and login to be simpified.

--- index.js
+++ index.js
@@ -5,27 +5,31 @@ let host = "https://sc2-rdops-vm06-dhcp-195-173";
 let username = 'administrator@vsphere.local';
 let password = 'Admin!23';
 
-async function listVms() {
-  return await request({ url: `${host}/rest/vcenter/vm` });
+async function get(url) {
+  return await request({ url: url });
 }
 
-// Basic Auth to vSphere REST Endpoint, uses cookie for authentication
-request({
-  url : `${host}/rest/com/vmware/cis/session`,
-  method: 'POST',
-  headers : {
-    "Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64")
-  }
-}).then((value) => {
-  listVms().then((vms) => { 
-    console.log(vms);
-  }, (error) => {
-    console.log(error);
+async function login() {
+  await request({
+    url : `${host}/rest/com/vmware/cis/session`,
+    method: 'POST',
+    headers : { "Authorization" : "Basic " + new Buffer(`${username}:${password}`).toString("base64") }
   });
-}, (error) => {
-  if(error.response) {
-    console.log(error.response.statusMessage);
-  } else {
-    console.log(error.message);
+}
+
+async function callApi() {
+  // Fetch a list of VMs
+  let vms = JSON.parse(await get(`${host}/rest/vcenter/vm`));
+  console.log(JSON.stringify(vms));
+  // Fetch details for the first vm
+  if(vms.value.length > 0) {
+    let vm = await get(`${host}/rest/vcenter/vm/${vms.value[0].vm}`);
+    console.log(vm);
   }
+}
+
+login().then(() => {
+  callApi();
+}, (error) => {
+  console.log(error);
 });
